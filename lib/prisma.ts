57a13a70debf1d06b8f37a182fc86873fe76d1ca
sebastien <sebastien.savan@gmail.com>
2025-03-27 09:06:34 +0100

    fix error client prisma during mep

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,6 @@ import { PrismaNeon } from "@prisma/adapter-neon"
 const connectionString = process.env.DATABASE_URL
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
-
 const prisma = new PrismaClient({ adapter })
 
 export default prisma
