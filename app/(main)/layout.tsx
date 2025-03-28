e7cc429fcb58dbf70ae985a3d38f71510b3e19b9
sebastien <sebastien.savan@gmail.com>
2025-03-28 07:47:49 +0100

    add user in bdd

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,31 @@
 import { Wrapper } from "@/components/Wrapper"
 import { Header } from "@/components/Header"
+import { currentUser } from "@clerk/nextjs/server"
+import prisma from "@/lib/prisma"
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const user = await currentUser()
+
+  if (!user) return null
+
+  const dbUser = await prisma.user.findUnique({
+    where: { clerkUserId: user.id },
+  })
+
+  if (!dbUser) {
+    await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    })
+  }
   return (
     <Wrapper>
       <Header />
