99a4f9ff691b8f37e631c04466a93c1b708d553e
sebastien <sebastien.savan@gmail.com>
2025-04-12 16:03:34 +0200

    margin signin and signup pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-    <div className='flex justify-center flex-1 mt-30'>
+    <div className='flex justify-center flex-1 mt-10'>
       <SignIn />
     </div>
   )
